69fbe652da5770063ebb670f8f019256353953ae
Elisha Peterson <triathematician@gmail.com>
2019-07-22 19:37:18 -0400

    update ReflectionUtils

--- firestarter/src/main/java/com/googlecode/blaisemath/util/ReflectionUtils.java
+++ firestarter/src/main/java/com/googlecode/blaisemath/util/ReflectionUtils.java
@@ -1,17 +1,17 @@
 package com.googlecode.blaisemath.util;
 
-/*
+/*-
  * #%L
- * Firestarter
+ * blaise-common
  * --
- * Copyright (C) 2009 - 2019 Elisha Peterson
+ * Copyright (C) 2014 - 2019 Elisha Peterson
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,21 +19,20 @@ package com.googlecode.blaisemath.util;
  * limitations under the License.
  * #L%
  */
-import java.beans.BeanInfo;
-import java.beans.IndexedPropertyDescriptor;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
+
+import java.beans.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Utility functions for working with reflection API, e.g. for catching
- * potential exceptions when invoking methods.
+ * Some general utilities for working with Java types.
  *
- * @author Elisha
+ * @author Elisha Peterson
  */
 public class ReflectionUtils {
 
@@ -42,7 +41,7 @@ public class ReflectionUtils {
     private static final String FAIL_NEW_MSG = "Failed to invoke constructor";
     private static final String FAIL_INVOKE_MSG = "Failed to invoke read method";
     private static final String NO_READ_MSG = "No read method available";
-    private static final String NO_WRITE_MSG = "No writemethod available";
+    private static final String NO_WRITE_MSG = "No write method available";
 
     // utility class
     private ReflectionUtils() {
@@ -62,7 +61,7 @@ public class ReflectionUtils {
         }
         return beanInfo;
     }
-    
+
     /**
      * Return an {@link IndexedPropertyDescriptor} for the specified object property.
      * @param cls the object class
@@ -77,8 +76,27 @@ public class ReflectionUtils {
                 return (IndexedPropertyDescriptor) pd;
             }
         }
-        throw new IllegalArgumentException("Unable to find property " + propName 
+        throw new IllegalArgumentException("Unable to find property " + propName
                 + " in the class " + cls);
+    }
+
+    /**
+     * Utility method to lookup a public static method, returning an empty optional if not found.
+     * @param type class type
+     * @param name method name
+     * @param paramTypes method parameters
+     * @return method if found
+     */
+    public static Optional<Method> findStaticMethod(Class<?> type, String name, Class... paramTypes) {
+        try {
+            Method m = type.getMethod(name, paramTypes);
+            if (Modifier.isStatic(m.getModifiers()) && Modifier.isPublic(m.getModifiers())) {
+                return Optional.of(m);
+            }
+        } catch (NoSuchMethodException | SecurityException ex) {
+            LOG.log(Level.FINEST, "Not found", ex);
+        }
+        return Optional.empty();
     }
 
     public static <T> T tryInvokeNew(Class<T> cls) {
@@ -176,4 +194,41 @@ public class ReflectionUtils {
         }
         return false;
     }
+
+    /**
+     * Utility method to lookup a public static method by one of several possible names, returning an empty optional if
+     * not found.
+     * @param type class type
+     * @param names possible names
+     * @param paramTypes method parameters
+     * @return method if found
+     */
+    public static Optional<Method> findStaticMethod(Class type, String[] names, Class... paramTypes) {
+        for (String n : names) {
+            Optional<Method> m = findStaticMethod(type, n, paramTypes);
+            if (m.isPresent()) {
+                return m;
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Utility method to lookup a constructor, returning an empty optional if not found.
+     * @param type class type
+     * @param paramTypes method parameters
+     * @return constructor if found
+     */
+    public static Optional<Constructor> findConstructor(Class<?> type, Class... paramTypes) {
+        try {
+            Constructor c = type.getConstructor(paramTypes);
+            if (Modifier.isPublic(c.getModifiers())) {
+                return Optional.of(c);
+            }
+        } catch (NoSuchMethodException | SecurityException ex) {
+            LOG.log(Level.FINEST, "Not found", ex);
+        }
+        return Optional.empty();
+    }
+
 }
